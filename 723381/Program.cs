2d3fa27ca16bb98425eed3e7012f844ffebdb663
visualstudio <visualstudio@122-PC21>
2019-02-06 10:02:42 -0500

    PRASTICE

--- 723381/Program.cs
+++ 723381/Program.cs
@@ -4,12 +4,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _723381
+namespace _723381_2
 {
     class Program
     {
         static void Main(string[] args)
         {
+            MethodA();
+        }
+
+        public static void MethodA()
+        {
+            while (Peanut())
+            {
+
+                Console.WriteLine(" i is {0}", i);
+                if (i > 10)
+                {
+                    return;
+                }
+
+            }
+        }
+
+        public static bool Peanut()
+        {
+            i++;
+            return true;
         }
     }
 }
